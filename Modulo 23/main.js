d575e4a84500a285d07fff57eab546b502cd2cb8
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-11-18 16:11:15 -0300

    Modulo 23 ok

---
+++ Modulo 23/main.js
@@ -0,0 +1,69 @@
+/*document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+        // AJAX - Asynchronous JavaScript and XML
+        const xhttp = new XMLHttpRequest();
+        const cep = document.getElementById('cep').value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
+
+        // https://viacep.com.br/ws/12312332/json
+    })
+})*/
+
+$(document).ready(function(){
+    $('#cep').mask('00000-000');
+
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val()
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this)
+        $(botao).find('i').addClass('d-none')
+        $(botao).find('span').removeClass('d-none')
+
+        /*$.ajax(endpoint).done(function(resposta){
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none')
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+                $('#endereco').val(endereco)
+            }, 1000)
+        })*/
+
+        fetch(endpoint)
+        .then(function(resposta){
+            return resposta.json()
+        })
+        .then(function(json){
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+
+        })
+        .catch(function(erro){
+            alert("Occorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+        })
+        .finally(function(){
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 500)
+        })
+    })
+
+    $('#formulario-pedido').submit(function(e){
+        e.preventDefault()
+        if ($('#nome').val().length == 0 ){
+            throw new Error("Digite o nome")
+        }
+    })
+})
