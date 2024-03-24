fee5bf9530ac0a504c755210085339a7c329a32a
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2024-03-23 23:56:06 -0300

    Loader added

--- Modulo 28 Exercicio/calculadoraIMC/src/App.jsx
+++ Modulo 28 Exercicio/calculadoraIMC/src/App.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
 import Titulo from "./components/Titulo";
 
 function App() {
+  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  },[])
+  
+
   return (
     <>
-      <Titulo />
-      <div className="container">
-        <Formulario />
+      {loading ? (
+      <>
+      <div className="center">
+        <div className="loader"></div>
       </div>
+      </>) : (
+    <>
+    <Titulo  />
+      <div className="container">
+        <Formulario/>
+      </div>
+    </>
+      )}
     </>
   )
 }
